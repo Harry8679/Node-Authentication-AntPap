0de15f1042a619dc09093df5af7d5a4c3dd80c04
Harry MacCode <emarh.harry.code@gmail.com>
2024-02-28 04:14:10 +0100

    Setup of Express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello world 2');
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+const port: Number = 8200;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello everyone');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on the port ${port}`);
+})
